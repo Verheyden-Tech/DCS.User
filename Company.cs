37c40c6fda2dd6a5ae57d4b5b7db38f8848c6bd5
ElCartmanBra420 <verheydentobias@gmail.com>
2024-09-10 11:59:17 +0200

    added company class

---
+++ Company.cs
@@ -0,0 +1,108 @@
+﻿namespace UserLibrary
+{
+    public class Company
+    {
+        private string name;
+        private string? phone;
+        private string? email;
+        private string? adress;
+        private string? city;
+        private int? postalcode;
+        private string? companytype;
+
+        public Company(string name, string phone, string email, string adress, string city, int postalcode, string companytype)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Adress = adress;
+            City = city;
+            Postalcode = postalcode;
+            Companytype = companytype;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string? Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string? Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string? Adress
+        {
+            get
+            {
+                return adress;
+            }
+            set
+            {
+                adress = value;
+            }
+        }
+
+        public string? City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+
+        public int? Postalcode
+        {
+            get
+            {
+                return postalcode;
+            }
+            set
+            {
+                postalcode = value;
+            }
+        }
+
+        public string? Companytype
+        {
+            get
+            {
+                return companytype;
+            }
+            set
+            {
+                companytype = value;
+            }
+        }
+    }
+}
