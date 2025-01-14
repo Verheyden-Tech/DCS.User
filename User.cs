e05913498e1c2ccfd75db00d7e22acc6648e529b
Convert.ToBias <158698008+ElCartmanBra420@users.noreply.github.com>
2025-01-14 18:44:05 +0100

    only user model and create service

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿namespace UserLibrary
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public Guid Guid { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public bool IsLoggedIn { get; set; }
+
+        public string ContactDataFilePath { get; set; }
+
+        public string CompanyDataFilePath { get; set; }
+
+        public string SaveFolderFilePath { get; set; }
+    }
+}
--- UserService.cs
+++ UserService.cs
@@ -9,6 +9,8 @@
             get { return instance; }
         }
 
+        private readonly User model;
+
         /// <summary>
         /// UserService contains methods for User-Account management.
         /// </summary>
@@ -20,106 +22,19 @@
         /// Creates new user instance.
         /// </summary>
         /// <returns>User instance.</returns>
-        public User CreateFullUser(string username, string password, bool isAdmin = false, string firstName = "", string lastName = "", string eMail = "", string adress = "", string city = "")
+        public User CreateUser(User user)
         {
             User newUser = new User
             {
                 Guid = Guid.NewGuid(),
-                UserName = username,
-                PassWord = password,
-                IsAdmin = isAdmin,
-                FirstName = firstName,
-                LastName = lastName,
-                Email = eMail,
-                Adress = adress,
-                City = city
+                UserName = user.UserName,
+                PassWord = user.PassWord,
+                IsAdmin = user.IsAdmin,
+                SaveFolderFilePath = user.SaveFolderFilePath,
+                ContactDataFilePath = user.ContactDataFilePath,
+                CompanyDataFilePath = user.CompanyDataFilePath
             };
             return newUser;
-        }
-
-        /// <summary>
-        /// Creates new user with basic account informations.
-        /// </summary>
-        /// <returns>Base user instance.</returns>
-        public User CreateBaseUser(string username, string password, bool isAdmin = false)
-        {
-            User newUser = new User
-            {
-                Guid = Guid.NewGuid(),
-                UserName = username,
-                PassWord = password,
-                IsAdmin = isAdmin
-            };
-            return newUser;
-        }
-
-        /// <summary>
-        /// Creates a new company instance.
-        /// </summary>
-        /// <returns>Company instance.</returns>
-        public Company CreateCompany(string name, string contact = "", string phone = "", string email = "", string adress = "", string city = "", string postalcode = "", string companytype = "")
-        {
-            Company newCompany = new Company
-            {
-                Guid = Guid.NewGuid(),
-                Name = name,
-                Contact = contact,
-                Phone = phone,
-                Email = email,
-                Adress = adress,
-                City = city,
-                Postalcode = postalcode,
-                Companytype = companytype
-            };
-            return newCompany;
-        }
-
-        /// <summary>
-        /// Creates a new customer instance.
-        /// </summary>
-        /// <returns>Customer instance.</returns>
-        public Contact CreateContact(string firstName = "", string lastName = "", string mailAddress = "", int phoneNumber = default, string adress = "", string city = "", int postalCode = default, string dataOwner = "", string? company = "", bool isActive = true)
-        {
-            Contact newContact = new Contact
-            {
-                Guid = Guid.NewGuid(),
-                FirstName = firstName,
-                LastName = lastName,
-                MailAddress = mailAddress,
-                PhoneNumber = phoneNumber,
-                Address = adress,
-                City = city,
-                PostalCode = postalCode,
-                DataOwner = dataOwner,
-                Company = company,
-                IsActive = isActive
-            };
-            return newContact;
-        }
-
-        public Group CreateGroup(string name, string description = "", bool isActive  = true)
-        {
-            Group newGroup = new Group
-            {
-                Guid = Guid.NewGuid(),
-                Name = name,
-                Description = description,
-                IsActive = isActive
-            };
-            return newGroup;
-        }
-
-        public Role CreateRole(string roleName, string description = "", bool isAdmin = false, bool isActive = true)
-        {
-            Role newRole = new Role
-            {
-                Guid = Guid.NewGuid(),
-                Name = roleName,
-                Description = description,
-                IsAdmin = isAdmin,
-                IsActive = isActive
-            };
-            return newRole;
         }
     }
 }
