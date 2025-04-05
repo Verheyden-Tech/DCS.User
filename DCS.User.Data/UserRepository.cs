95903c4e6a52370d6a616bc6207ddeccd13382f9
Convert.ToBias <158698008+ElCartmanBra420@users.noreply.github.com>
2025-04-05 11:32:44 +0200

    Update project versions and dependencies across modules
    - Bump `DCS.User.Data` version to `1.0.601.0` and update `AssemblyVersion`.
    - Upgrade `DCS.CoreLib.BaseClass` to version `1.0.201` in multiple projects.
    - Restructure `DocumentLayout` and `DocumentLayout.backup.json`, removing and adding document entries.
    - Update `DocumentLayout.json` with new entries for `UserManagementViewModel.cs` and `RegistrateUserViewModel.cs`.
    - Update `DCS.User.Service` to version `1.0.601` with corresponding dependency changes.
    - Update `DCS.User.UI` to version `1.0.401` and adjust dependencies accordingly.
    - Revise `.nuspec` files for `DCS.User.Data` and `DCS.User.Service` to reflect new versions.
    - Update `sourcelink.json` files to point to new commit hashes.
    - Various binary files (DLLs, PDBs) have been rebuilt or modified.
    - Refresh `project.assets.json` files to reflect updated package versions.
    - Update `AssemblyInfo.cs` files with new version numbers and attributes.
    - Revise `.deps.json` files for `DCS.User.Data` and `DCS.User.Service` to reflect updated dependencies.
    - Similar updates made to `DCS.User.UI` project structure and dependencies.

--- DCS.User.Data/UserRepository.cs
+++ DCS.User.Data/UserRepository.cs
@@ -13,18 +13,18 @@ namespace DCS.User.Data
         /// <summary>
         /// Tablename for the UserRepository.
         /// </summary>
-        private static string TableName => "dbo.VT_User";
+        private static string tableName => "dbo.VT_User";
 
         /// <summary>
         /// PrimaryKeyColumn to identify users on the table.
         /// </summary>
-        private static string PrimaryKeyColumn => "Guid";
+        private static string primaryKeyColumn => "Guid";
 
         /// <summary>
         /// Default constructor for UserRepository.
         /// </summary>
         /// <param name="sqlService">Sql service.</param>
-        public UserRepository(ISqlService sqlService) : base(sqlService)
+        public UserRepository(ISqlService sqlService) : base(sqlService, tableName, primaryKeyColumn)
         {
             this.sqlService = sqlService;
         }
