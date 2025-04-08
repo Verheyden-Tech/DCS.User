39d6fc2caa14a93768f2ddc7ad6bf98fd0ef3a72
Convert.ToBias <158698008+ElCartmanBra420@users.noreply.github.com>
2025-04-08 14:39:16 +0200

    Update project to version 1.0.801.0 release
    - Bump version and assembly numbers for DCS.User.Data,
      DCS.User.Service, and DCS.User.UI to 1.0.801.0.
    - Update dependencies in .deps.json files to reflect new
      version numbers.
    - Add new .nuspec files for updated packages.
    - Modify DocumentLayout.json and backup files to include
      new documents: UserRepository.cs and UserService.cs.
    - Retain WhenOpened timestamps for documents.
    - Update sourcelink.json to point to the latest commit.
    - Change resource URIs in views to reflect new version.
    - Update project.assets.json with new dependency paths.
    - Revise AssemblyInfo.cs files with new version info.
    - Add GlobalUsings.g.cs and GeneratedMSBuildEditorConfig.editorconfig
      for modern project structure.

--- DCS.User.UI/obj/Debug/net7.0-windows/Views/UserLogin.g.i.cs
+++ DCS.User.UI/obj/Debug/net7.0-windows/Views/UserLogin.g.i.cs
@@ -158,7 +158,7 @@ namespace DCS.User.UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DCS.User.UI;V1.0.701.0;component/views/userlogin.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DCS.User.UI;V1.0.801.0;component/views/userlogin.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\UserLogin.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
